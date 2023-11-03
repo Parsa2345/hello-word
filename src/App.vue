507580d11f62777771f490d228e4fc9b8d50b0a7
Parsa2345 <jack.kamard@gmail.com>
2023-11-03 16:08:52 +0100

    ,kjbhbil

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,66 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+
+<label for="namr"> Enter Name</label>
+<input type="text" ref="newName"/>
+<input type="submit" @click="test" />
+
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <!-- <MyHelloWord :name="name" :family="family" :age="age" @nameClicked="test" /> -->
+  <!-- <MyHelloWord
+    @click="test"
+    :name="name"
+    :family="family"
+    :age="age"
+    ref="MyHelloWordComponent"
+  >
+    <template v-slot:Mohsen>
+      <h1>Mohsen</h1>
+    </template> 
+<template v-slot:parsa>
+  <h1>parsa</h1>
+</template>
+<template v-slot:age>
+  <h1>30</h1>
+</template>
+ 
+    <slot>lk</slot>
+</MyHelloWord> -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+// import MyHelloWord from "./components/MyHelloWord.vue";
 
 export default {
-  name: 'App',
+  data() {
+    return {
+      name: "Mohsen",
+      family: "Jafari",
+      age: 20,
+    };
+  },
+  methods: {
+    test() {
+      // console.log("test" +" "+text);
+      // this.$refs.MyHelloWordComponent.alertname();
+     this.name= this.$refs.newName.value;
+    },
+  },
+  watch :{
+name(newValue,oldValue)
+{
+console.log(oldValue);
+console.log(newValue);
+},
+  },
+
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    // HelloWorld
+    // MyHelloWord,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/MyHelloWord.vue
@@ -0,0 +1,33 @@
+<template>
+    <!-- <h1 >{{name}} {{family}} {{age}}</h1> -->
+
+<slot name="Mohsen"></slot>
+<slot name="parsa"></slot>
+<slot name="age"></slot>
+</template>
+
+
+
+<script>
+
+export default ({
+
+    props :{
+        name:String,
+        family:String,
+        age:Number
+    },
+    methods: {
+        alertname(){
+            alert(this.name);
+            // this.$emit("nameClicked",this.name);
+        }
+    },
+ })
+</script>
+
+<style scoped>
+  h1{
+    color: blue;
+  }
+</style>
